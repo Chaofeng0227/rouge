1a1f7825e7bd6fed254ceb9dfb3453bdc7c54dda
a13242854007 <a13242854007@gmail.com>
2026-03-22 02:16:44 +0800

    Initial Unity project import

---
+++ Assets/MonsterSpawner.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public float spawnInterval = 2f;
+    public float spawnRadius = 0.5f;
+    public int maxEnemyCount = 20;
+
+    private float timer;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= spawnInterval)
+        {
+            timer = 0f;
+
+            if (GameObject.FindGameObjectsWithTag("Enemy").Length < maxEnemyCount)
+            {
+                SpawnEnemy();
+            }
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        if (enemyPrefab == null) return;
+
+        Vector2 randomOffset = Random.insideUnitCircle * spawnRadius;
+        Vector3 spawnPos = transform.position + new Vector3(randomOffset.x, randomOffset.y, 0f);
+
+        Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
+    }
+}
---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 10;
+    private int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        Debug.Log("玩家初始血量: " + currentHealth);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+
+        Debug.Log("玩家受到伤害: " + damage + "，当前血量: " + currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("玩家血量为 0，游戏结束");
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
